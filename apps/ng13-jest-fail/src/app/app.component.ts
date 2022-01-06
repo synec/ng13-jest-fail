bffa49da40b05f17efc8df0e70b596c34ba2785b
Daniel Grabert <dg@indivirtuell.net>
2022-01-06 11:47:15 +0100

    Fail with lodash-es

--- apps/ng13-jest-fail/src/app/app.component.ts
+++ apps/ng13-jest-fail/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { isObject } from 'lodash-es';
 
 @Component({
   selector: 'ng13-jest-fail-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng13-jest-fail';
+
+  ngOnInit(): void {
+    console.log(isObject(this));
+  }
+
 }
