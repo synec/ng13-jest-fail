971d782af5e9c43c797ed6860e076c7b20758ec5
Daniel Grabert <dg@indivirtuell.net>
2022-01-06 11:56:10 +0100

    Fail with minimal akita store

--- apps/ng13-jest-fail/src/app/app.component.ts
+++ apps/ng13-jest-fail/src/app/app.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { isObject } from 'lodash-es';
+import { Query, Store } from '@datorama/akita';
+
+interface TestState {
+  valid: boolean;
+}
+
+export class TestStore extends Store<TestState> {
+  constructor() {
+    super({
+      valid: false,
+    })
+  }
+}
+
+export class TestQuery extends Query<TestState> {
+  constructor(
+    override readonly store: TestStore,
+  ) {
+    super(store);
+  }
+}
 
 @Component({
   selector: 'ng13-jest-fail-root',
@@ -8,6 +29,10 @@ import { isObject } from 'lodash-es';
 })
 export class AppComponent implements OnInit {
   title = 'ng13-jest-fail';
+
+  constructor(
+    readonly query: TestQuery,
+  ) {}
 
   ngOnInit(): void {
     console.log(isObject(this));
