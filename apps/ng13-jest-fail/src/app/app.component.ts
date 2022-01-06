f807d74ad8106b1dbb295d8a9f700c5859dcfd83
Daniel Grabert <dg@indivirtuell.net>
2022-01-06 11:40:24 +0100

    initial commit

---
+++ apps/ng13-jest-fail/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng13-jest-fail-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng13-jest-fail';
+}
